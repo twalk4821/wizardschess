4b4d7ab45bcd328c7182efb9ae5a7d88129fc1a6
Tyler Walker <tyjohnwalker@gmail.com>
2017-09-05 18:32:13 -0700

    image assets

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,4 +1,31 @@
 import React, { Component } from 'react';
+import blackbishop from "../assets/blackbishop.png"
+import whitebishop from "../assets/whitebishop.png"
+import blackknight from "../assets/blackknight.png"
+import whiteknight from "../assets/whiteknight.png"
+import blackqueen from "../assets/blackqueen.png"
+import whitequeen from "../assets/whitequeen.png"
+import blackking from "../assets/blackking.png"
+import whiteking from "../assets/whiteking.png"
+import blackpawn from "../assets/blackpawn.png"
+import whitepawn from "../assets/whitepawn.png"
+import blackrook from "../assets/blackrook.png"
+import whiterook from "../assets/whiterook.png"
+
+const images = {
+	blackbishop : blackbishop,
+whitebishop : whitebishop,
+blackknight : blackknight,
+whiteknight : whiteknight,
+blackqueen : blackqueen,
+whitequeen : whitequeen,
+blackking : blackking,
+whiteking : whiteking,
+blackpawn : blackpawn,
+whitepawn : whitepawn,
+blackrook : blackrook,
+whiterook : whiterook
+}
 
 class Square extends Component {
 	constructor(props) {
@@ -20,10 +47,15 @@ class Square extends Component {
 	}
 
 	render() {
+		if (this.props.piece) {
+			console.log("../assets/" + this.props.piece.color + this.props.piece.type + ".png")
+		}
 		var classes = this.state.active ? "square active " : "square " + (this.props.piece ? this.props.piece.color : "")
 		return (
 			<div className={classes} onClick={() => this.props.toggle(this)}>
-				{this.props.piece ? this.props.piece.type : null}
+				{this.props.piece &&
+					<img src={images[this.props.piece.color + this.props.piece.type]} height="50" width="50"/>
+				}
 			</div>
 		)
 	}
