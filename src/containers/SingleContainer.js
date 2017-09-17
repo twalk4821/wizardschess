cfe6390c87028dfe379984eb630ce1c9182c93fe
Michael Burton <michaelburton@Michaels-MacBook-Pro.local>
2017-09-17 01:46:05 -0700

    begun styling on Local container

--- src/containers/SingleContainer.js
+++ src/containers/SingleContainer.js
@@ -1,21 +1,72 @@
 import React, { Component } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { Paper } from 'material-ui/Paper';
 
 class SingleContainer extends Component {
 	constructor(props) {
 	  super(props)
-	  this.state = {}
+	  this.state = {
+	  	white: "Harry",
+	  	black: "Draco",
+	  	redirect: false
+	  }
 	  //bind functions
+	  this.handleChange = this.handleChange.bind(this);
+	  this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
+	handleChange(e, color) {
+		console.log(e, color)
+		switch (color) {
+			case "black":
+				this.setState({
+					black: e.target.value
+				})
+				break;
+			case "white":
+				this.setState({
+					white: e.target.value
+				})
+			default:
+				break;
+		}	
+	}
+
+	handleSubmit() {
+		let names = {
+        white: this.state.white,
+        black: this.state.black
+      };
+		this.props.updatePlayerNames(names);
+		this.setState({
+			redirect: true
+		})
+	}
 
 	render(){
 	  return(
 	  	<div>
-	  	<div className="App-header">
-	  	  <h2>Wizards Chess</h2>
-	  	</div>
-	    <h1>single</h1>
-	    </div>
+		  	<div className="App-header">
+		  	  <h2>Wizards Chess</h2>
+		  	</div>
+		  	<form onSubmit={this.handleSubmit} >
+		  	  <h2>Player 1: 
+		    	  <input className="inputName" value={this.state.white} onChange={(e) => {this.handleChange(e, "white")}} />
+	    	  </h2>
+	    	  <h2> Player 2:
+				  	<input className="inputName" value={this.state.black} onChange={(e) => {this.handleChange(e, "black")}} /> 
+			  	</h2>
+			  	<h2>
+				  	<input className="submit" type="submit" value="Start Game"/> 
+				  </h2>
+				      <Paper style={style} zDepth={5} rounded={false} />
+
+	   	  </form>
+	    
+		    {this.state.redirect &&
+		    	<Redirect to="/game" />
+		    }
+	   </div>
 	  )
 	}
 }
